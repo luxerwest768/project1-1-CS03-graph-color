a873491f27bbb72fa757c505e7c0097318482d32
FMGY0nko <theevilrabbit999@gmail.com>
2024-10-21 17:04:13 +0200

    Added comments and cleaned it up

--- WelshAlgorithm.java
+++ WelshAlgorithm.java
@@ -1,10 +1,8 @@
 import java.util.*;
 public class WelshAlgorithm {
-    public static void main(String[] args) {
-        ReadGraph.main(args);
-
-
-    }
+    /* 
+     * NOT FINISHED YET. DO NOT RUN
+    */
 
     // Method that returns an order list of each vertex's degree
     public static int[][] degreeList(int nOfVertices, ColVertices[] v) {
@@ -13,7 +11,7 @@ public class WelshAlgorithm {
 
         // Adds the vertex id and degree to a matrix
         for(int i = 0; i < nOfVertices; i++) {
-            temp[i][0] = i + 1;
+            temp[i][0] = i;
             temp[i][1] = v[i].degree;
         }
 
@@ -29,13 +27,74 @@ public class WelshAlgorithm {
         return degreelist;
     }
 
-    public static void colouring(int[][] degreeList, ColVertices[] v) {
+    public static int chromaticNumber(int[][] degreeList, ColVertices[] v, int nOfVertices) {
+        // Initializes the colour and finished variable. 
         int colour = 1; 
         boolean finished = false;
 
+        // Only stops loops once each vertex is coloured
         while(finished == false) {
-            
+            // Loops through each vertex
+            for(int i = 0; i < nOfVertices; i++) {
+                // Checks whether its been coloured
+                if(v[degreeList[i][0]].colour == 0) {
+                    // Checks whether a neighbour has the same colour
+                    if(canColour(v, v[degreeList[i][0]], colour)) {
+                        // Colours the vertex if no neighbour has the same colour
+                        v[degreeList[i][0]].colour = colour;
+                    }
+                }
+            }
+
+            // Checks whether each vertex has been coloured
+            finished = done(v, nOfVertices);
         }
 
+        return colour;
+    }
+
+    public static boolean canColour(ColVertices[] v,ColVertices vertex, int colour) {
+        boolean isFine = true;
+
+        // Loops through each edge connected to the vertex
+        for(ColEdge edge : vertex.edges) {
+            // If the vertex in u is not the one we are looking at
+            if(edge.u != vertex.id) {
+                // If the vertex connected has the same colour we are looking at then it is no longer fine
+                if(v[edge.u - 1].colour == colour) {
+                    isFine = false;
+                }
+            }
+            // If the vertex in u is not the one we are looking at
+            else if(edge.v != vertex.id) {
+                // If the vertex connected has the same colour we are looking at then it is no longer fine
+                if(v[edge.v - 1].colour == colour) {
+                    isFine = false;
+                }
+            }
+        }
+
+        return isFine;
+    }
+
+    public static boolean done(ColVertices[] v, int nOfVertices) {
+        boolean done = false;
+        // n is the number of coloured vertices
+        int n = 0;
+
+        // Loops through each vertex
+        for(int i = 0; i < nOfVertices; i++) {
+            // Checks whether they have been coloured
+            if(v[i].colour != 0) {
+                n += 1;
+            }
+        }
+
+        // Checks whether the number of coloured vertices is equal to the total vertices 
+        if(n == nOfVertices) {
+            done = true;
+        }
+
+        return done;
     }
 }
--- textTranslation.java
+++ textTranslation.java
@@ -7,10 +7,14 @@ public class textTranslation {
         for(int i = 0; i < nOfVertices; i++) {
             // Loopes through each edge the vertice is connected to
             for(ColEdge edge : v[i].edges) {
+                // If the edge.u does not equal the vertex being looked at
                 if(edge.u != (i + 1)) {
+                    // Then it shows in the matrix that it is connected to the vertex being looked at
                     graph[i][(edge.u - 1)] = 1;
                 }
+                // If the edge.v does not equal the vertex being looked at
                 else if(edge.v != (i + 1)) {
+                    // Then it shows in the matrix that it is connected to the vertex being looked at
                     graph[i][(edge.v - 1)] = 1;
                 }
             }
