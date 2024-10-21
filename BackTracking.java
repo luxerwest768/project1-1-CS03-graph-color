b1aafc787c4be5c1dfb18b2b717fe36f5ba3ff17
BachMinh <bachdangthaiminh@gmail.com>
2024-10-21 18:17:01 +0200

    add: comment to BackTracking.java

--- BackTracking.java
+++ BackTracking.java
@@ -1,12 +1,13 @@
 
-import java.util.Arrays;
-
-public class CountChromaticNum {
+public class BackTracking {
     // checking if is there any vertices next to have a same color
     static boolean isSafe (int vertex, int[][] graph, int color, int[] colorSet){
+        // loop through all the neighbor vertices of that vertex
         for (int neighborEdge = 0; neighborEdge < graph[vertex-1].length ; neighborEdge++){
-            if (graph[vertex-1][neighborEdge] == 1){ // there is an edge between 2 vertices
-                if (colorSet[neighborEdge] == color ){ // if the neighbor vertex has the same color, return false
+            // check there is an edge between 2 vertices
+            if (graph[vertex-1][neighborEdge] == 1){
+                 // check if the neighbor vertex has the same color
+                if (colorSet[neighborEdge] == color ){
                     return false;
                 }
             }
@@ -17,17 +18,20 @@ public class CountChromaticNum {
     
     // color every vertices
     static boolean graphColorUntil (int vertex, int[][] graph, int[] colorSet, int numColor){
-        if (vertex == graph.length+1){ // every vertices have been colored
+        // return when every vertices have been colored
+        if (vertex == graph.length+1){
             return true; 
         }
+        // loop through all the color in color set
         for (int color = 0; color < numColor; color++){
-            if (isSafe(vertex, graph, color, colorSet)){
+            if (isSafe(vertex, graph, color, colorSet)){ //
                 colorSet[vertex-1] = color; // assign the color to uncolored vertex
-                if (graphColorUntil(vertex+1, graph, colorSet,numColor)){ // move to the next vertex
+                 // move to the next vertex
+                if (graphColorUntil(vertex+1, graph, colorSet,numColor)){
                     return true;
                 }
 
-                // backtracking, if the color does not lead to solution
+                // backtracking, if the current color does not lead to solution
                 colorSet[vertex-1] = -1;
             }
 
@@ -38,8 +42,10 @@ public class CountChromaticNum {
     // main method for graph coloring
     static int graphColoring (int[][] graph){
         int[] colorSet = new int[graph.length];
+        // limit the number of color can be used for backtracking
         for (int numColor=1; numColor<graph.length+1; numColor++){
-            Arrays.fill(colorSet, -1); // reset every vertices to uncolored
+            fill(colorSet, -1); // reset every vertices to uncolored
+            // if all the vertices is colored, return the number of color that is used
             if (graphColorUntil(1, graph, colorSet,numColor)){
                 return numColor;
             }
@@ -47,6 +53,14 @@ public class CountChromaticNum {
         System.out.println("Solution does not exist");
         return graph.length; // return the worst case when all vertices colored with different color
 
+     }
+
+    
+    // assign every elements in array with k
+    static void fill(int[] arr, int k){
+        for (int i=0; i<arr.length; i++){
+            arr[i] = k;
+        }
     }
 
     public static void main(String[] args) {
--- ReadGraph.java
+++ ReadGraph.java
@@ -174,7 +174,7 @@ public class ReadGraph{
         */
 
         // Gets the chromatic number via the backtracking algorithm
-        int chromaticNumber = CountChromaticNum.graphColoring(graph);
+        int chromaticNumber = BackTracking.graphColoring(graph);
         // Prints out the chromatic number
         System.out.println("Chromatic Number: " + chromaticNumber);
     }
