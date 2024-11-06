7fe397188de3e3913f35c0156f4e67a399ed3eed
luxerwest768 <76089487+luxerwest768@users.noreply.github.com>
2024-11-06 18:26:41 +0100

    2 more interfaces created

--- Java FX/Display/src/main/java/com/example/display/Controller.java
+++ Java FX/Display/src/main/java/com/example/display/Controller.java
@@ -1,12 +1,10 @@
 package com.example.display;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -16,15 +14,31 @@ public class Controller {
     private Scene scene;
     private Parent root;
 
-    public void backToWelcome(ActionEvent event) throws IOException {
+    public void welcome(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("welcome.fxml"));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
-    public void switchMainMenu(ActionEvent event) throws IOException {
+    public void mainMenu(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("mainMenu.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void selectGraph(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("selectGraph.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void createGraphMenu(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("createGraphMenu.fxml"));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
