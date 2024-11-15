8991ac20b4e6959a4a137c6d7b12adb0e9cf6d16
BachMinh <bachdangthaiminh@gmail.com>
2024-11-15 02:35:09 +0100

    add: main page

--- javafx/src/App.java
+++ javafx/src/App.java
@@ -2,28 +2,32 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.scene.layout.VBox;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.text.Text;
 
 public class App extends Application {
     @Override
     public void start(Stage primaryStage){
         Button btn = new Button();
-        btn.setText("Say Hello World!");
+        Text txt = new Text("                        Welcome to \n Three Musketeers - Graph Coloring Game");
+        btn.getStyleClass().add("play-button");
+        txt.getStyleClass().add("txt");
+        btn.setText("Play");
         btn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event){
-                System.out.println("Hello world");
+                System.out.println("Let Play");
             }
         });
 
         StackPane root = new StackPane();
-        root.getChildren().add(btn);
-
-        Scene scene = new Scene(root,300,250);
-
-        primaryStage.setTitle("Hello world");
+        root.getChildren().addAll(txt,btn);
+        Scene scene = new Scene(root,1300,1000);
+        scene.getStylesheets().addAll("./css/homePage.css");
+        primaryStage.setTitle("Three Musketeers - Graph Coloring Game");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
