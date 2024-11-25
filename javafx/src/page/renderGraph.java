75adb6c36bfdeaf6a3b9409f3b1e565c6c580319
BachMinh <bachdangthaiminh@gmail.com>
2024-11-25 01:58:28 +0100

    fea: change location of edges

--- javafx/src/page/renderGraph.java
+++ javafx/src/page/renderGraph.java
@@ -27,14 +27,27 @@ public class renderGraph {
 
         Button renderButton = new Button("Render");
         renderButton.getStyleClass().add("render-button");
-        pane.getChildren().add(renderButton);
+        Button backButton = new Button("Back");
+        backButton.getStyleClass().add("back-button");
+        backButton.setOnAction((ActionEvent e) -> {
+            App.changeSelectGameScene();
+        });
+        pane.getChildren().addAll(renderButton,backButton);
 
+
+
+        // make an event to reload edge's location whenever change node's location
         EventHandler<ActionEvent> render = new EventHandler<ActionEvent>() {
             public void handle(ActionEvent event) {
                 for (int i = 0; i < edgeSet.getNumEdges(); i++) {
                     pane.getChildren().remove(edgeSet.getEdge(i));
                 }
                 edgeSet.removeAllEdges();
+                for (int i=0 ; i<numVertices ; i++){
+                    Vertex node = nodeSet.getVertex(i);
+                    edgeSet.setLocationX(i,node.getX());
+                    edgeSet.setLocationY(i,node.getY());
+                }
                 for (int i = 0; i < numVertices; i++) {
                     for (int j = 0; j < numVertices; j++) {
                         if (graph[i][j] == 1) {
@@ -52,6 +65,15 @@ public class renderGraph {
         };
         renderButton.setOnAction(render);
 
+        // render nodes
+        for (int i = 0; i < numVertices; i++) {
+            Vertex node = new Vertex();
+            nodeSet.addVertex(node);
+            node.setPosition(edgeSet.getLocationX(i), edgeSet.getLocationY(i));
+            node.drag(); // make node can be mouseDrag
+            pane.getChildren().add(node.getCircle());
+        }
+
         // render edges
         for (int i = 0; i < numVertices; i++) {
             for (int j = 0; j < numVertices; j++) {
@@ -65,15 +87,6 @@ public class renderGraph {
                     pane.getChildren().add(edge);
                 }
             }
-        }
-
-        // render nodes
-        for (int i = 0; i < numVertices; i++) {
-            Vertex node = new Vertex();
-            nodeSet.addVertex(node);
-            node.setPosition(edgeSet.getLocationX(i), edgeSet.getLocationY(i));
-            node.drag(); // make node can be mouseDrag
-            pane.getChildren().add(node.getCircle());
         }
 
         Scene scene = new Scene(pane, 1200, 900);
